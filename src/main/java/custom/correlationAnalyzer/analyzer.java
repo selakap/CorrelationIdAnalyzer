2faa25a80f96c1808aa229d7e86044d60a9f70e7
selakap <selaka.sp@gmail.com>
2023-02-07 12:30:49 +0530

    new logic

--- src/main/java/custom/correlationAnalyzer/analyzer.java
+++ src/main/java/custom/correlationAnalyzer/analyzer.java
@@ -1,21 +1,23 @@
 package custom.correlationAnalyzer;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 
 public class analyzer implements Runnable {
-    public static List<Object> nonmatchedcorrelationIdList = new ArrayList();;
+    public static List<Object> nonmatchedcorrelationIdList = new ArrayList();
     private Object correlationIdentifier;
     public static List correlationIdList = new ArrayList();//To store the correlation ids in correlation.log file
-
+    //public static HashMap<String, String> correlationIdMap = new HashMap<String, String>();//To store the correlation ids in correlation.log file
+    public static List<Object> delayedcorrelationIdList = new ArrayList();
     public analyzer(Object Id) {
         this.correlationIdentifier = Id;
-        // store parameter for later user
+        // store parameter for later use
     }
 
     public static void main(String[] args) {
@@ -25,8 +27,9 @@ public class analyzer implements Runnable {
             try {
                 File myObj = new File("/Users/selakapiumal/Desktop/correlatioAnalyzer/correlationIds.txt");
                 File myObj1 = new File("/Users/selakapiumal/Desktop/correlatioAnalyzer/nonmatchedcorrelationIds.txt");
+                File myObj2 = new File("/Users/selakapiumal/Desktop/correlatioAnalyzer/delayedcorrelationIds.txt");
 
-                if (myObj.createNewFile()&&myObj1.createNewFile()) {
+                if (myObj.createNewFile()&&myObj1.createNewFile()&&myObj2.createNewFile()) {
                     System.out.println("File created: " + myObj.getName());
                 } else {
                     System.out.println("File already exists.");
@@ -46,12 +49,13 @@ public class analyzer implements Runnable {
 
             /* read log line by line */
             while ((strLine = br.readLine()) != null)   {
-                if (strLine.split("\\|").length > 5 ){
+                if (strLine.split("\\|").length > 5 && strLine.contains("ROUND-TRIP LATENCY")){
 
                     String correlationId = (strLine.split("\\|"))[1];
                     System.out.println(correlationId);
                     if (!correlationIdList.contains(correlationId)){
-                        correlationIdList.add(correlationId);
+                        //store correlationId,timestamp
+                        correlationIdList.add(correlationId+","+(strLine.split("\\|"))[0]);
                     }
                 }
             }
@@ -90,17 +94,32 @@ public class analyzer implements Runnable {
     @Override
     public void run() {
         //System.out.println("inside run" + correlationIdentifier.toString());
-        String grepCommand = "grep " + correlationIdentifier.toString() +" /Users/selakapiumal/Desktop/correlatioAnalyzer/wso2carbon.log";
+        String grepCommand = "grep " + correlationIdentifier.toString().split(",")[0] +" /Users/selakapiumal/Desktop/correlatioAnalyzer/wso2carbon.log";
         Process proc = null;
 
 
         try {
+            //execute the f=grep command and read the output
             proc = Runtime.getRuntime().exec(grepCommand);
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(proc.getInputStream()));
             String line = "";
+
+            SimpleDateFormat parser = new SimpleDateFormat("HH:mm:ss");
             if ((line = reader.readLine()) != null){
                 System.out.print("existing id" + "\n");
+                //If correlation id exist in the Gw carbon log (as a time out)
+                // then check the time difference of mi correlation and Gw caron log
+                //if the difference is greater than 50s we are assuming it as a missing request
+                if (line.contains("timeout") || line.contains("Timeout")){
+                    long gwCarbonlogTime = parser.parse(line.split(" ")[4].split("]")[0]).getTime();
+                    long miCorrelationLogTime = parser.parse(correlationIdentifier.toString().split(",")[1].split(" ")[1].split(",")[0]).getTime();
+
+                    if ((((gwCarbonlogTime-miCorrelationLogTime)/1000)% 60) >= 50){
+                        delayedcorrelationIdList.add(correlationIdentifier.toString().split(",")[0]);
+
+                    }
+                }
 
             }else {
                 nonmatchedcorrelationIdList.add(correlationIdentifier.toString());
@@ -108,16 +127,22 @@ public class analyzer implements Runnable {
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
         if (correlationIdentifier == correlationIdList.get(correlationIdList.size() - 1)){
             System.out.println("Non matching ids:  " +nonmatchedcorrelationIdList);
+            System.out.println("delayed matching ids:  " +delayedcorrelationIdList);
             //Save non matching ids ===============================================================================
 
             try {
                 FileWriter myWriter1 = new FileWriter("/Users/selakapiumal/Desktop/correlatioAnalyzer/nonmatchedcorrelationIds.txt");
                 myWriter1.write(nonmatchedcorrelationIdList.toString());
                 myWriter1.close();
+                FileWriter myWriter2 = new FileWriter("/Users/selakapiumal/Desktop/correlatioAnalyzer/delayedcorrelationIds.txt");
+                myWriter2.write(delayedcorrelationIdList.toString());
+                myWriter2.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
